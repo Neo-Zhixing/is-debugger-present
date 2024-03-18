12c401e0a96dc59104de24b3bc742c11f11b3e6c
Zhixing Zhang <me@neoto.xin>
2024-03-18 16:43:04 -0700

    Implement for macOS

--- src/lib.rs
+++ src/lib.rs
@@ -30,11 +30,43 @@ pub fn is_debugger_present() -> bool {
         return false;
     }
 
+    #[cfg(target_os = "macos")]
+    unsafe {
+        let mut mib = [
+            libc::CTL_KERN,
+            libc::KERN_PROC,
+            libc::KERN_PROC_PID,
+            libc::getpid(),
+        ];
+        #[allow(non_camel_case_types)]
+        #[repr(C)]
+        struct kinfo_proc {
+            padding: [usize; 4],
+            p_flag: libc::c_int,
+            more_padding: i32,
+            rest: [u8; 608],
+        }
+        let mut info: kinfo_proc = std::mem::zeroed();
+        let mut size = std::mem::size_of::<kinfo_proc>();
+        assert_eq!(size, 648);
+        libc::sysctl(
+            mib.as_mut_ptr(),
+            4,
+            &mut info as *mut kinfo_proc as *mut _,
+            &mut size,
+            std::ptr::null_mut(),
+            0,
+        );
+        const P_TRACED: libc::c_int = 0x00000800;
+        return (info.p_flag & P_TRACED) != 0;
+    }
+
     #[allow(unreachable_code)]
     false
 }
 
 pub fn breakpoint() {
+    #![allow(unreachable_code)]
     if !is_debugger_present() {
         return;
     }
@@ -43,6 +75,14 @@ pub fn breakpoint() {
         unsafe {
             windows_sys::Win32::System::Diagnostics::Debug::DebugBreak();
         }
+        return;
+    }
+    #[cfg(target_arch = "x86_64")]
+    {
+        unsafe {
+            std::arch::asm!("int3");
+        }
+        return;
     }
 
     #[cfg(not(target_os = "windows"))]
@@ -50,16 +90,5 @@ pub fn breakpoint() {
         unsafe {
             libc::raise(libc::SIGTRAP);
         }
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use crate::is_debugger_present;
-
-    #[test]
-    fn my_test() {
-        let present = is_debugger_present();
-        println!("Present: {:?}", present);
     }
 }
